eef351a0fc03fd29d06faf2a46935b99d3e6b41c
2sseong <sh999051@naver.com>
2025-03-20 16:46:17 +0900

    8일차 커밋 마무리

--- toyproject/madang_작업쿼리.sql
+++ toyproject/madang_작업쿼리.sql
@@ -9,6 +9,28 @@ INSERT INTO Students (std_id, std_name, std_mobile, std_regyear)
 
 INSERT INTO Students (std_id, std_name, std_mobile, std_regyear) 
 	 VALUES (seq_student.nextval, '홍길순', '010-0000-9998', 2000);
-
+ 
 COMMIT;
 ROLLBACK;
+
+DELETE FROM students;
+
+SELECT std_id, std_name
+	 , std_mobile, std_regyear
+  FROM students;
+
+INSERT INTO Students (std_id, std_name, std_mobile, std_regyear) 
+	 VALUES (1, '홍길동', '010-0000-9999', 1997);
+
+INSERT INTO Students (std_id, std_name, std_mobile, std_regyear) 
+	 VALUES (2, '홍길순', '010-0000-9998', 2000);
+
+UPDATE students
+SET std_id =1
+WHERE std_name ='홍길동';
+
+
+INSERT INTO MADANG.STUDENTS
+(STD_ID, STD_NAME, STD_MOBILE, STD_REGYEAR)
+VALUES(0, '', '', 0);
+
